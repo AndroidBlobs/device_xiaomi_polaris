24d9c99592d459d642435388d476db50f9c4b537
ShivamKumarJha <jha.shivam3@gmail.com>
2020-02-11 01:36:12 +0530

    polaris-user-10-QKQ1.190828.002-V11.0.3.0.QDGMIXM-release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- lineage_polaris.mk
+++ lineage_polaris.mk
@@ -12,12 +12,12 @@ PRODUCT_BRAND := xiaomi
 PRODUCT_DEVICE := polaris
 PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_NAME := lineage_polaris
-PRODUCT_MODEL := MIX 2S
+PRODUCT_MODEL := Mi MIX 2S
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := polaris
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="polaris-user 9 PKQ1.180729.001 V11.0.2.0.PDGCNXM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="polaris-user 10 QKQ1.190828.002 V11.0.3.0.QDGMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/polaris/polaris:9/PKQ1.180729.001/V11.0.2.0.PDGCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/polaris/polaris:10/QKQ1.190828.002/V11.0.3.0.QDGMIXM:user/release-keys
